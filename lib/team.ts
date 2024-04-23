bcdb11f4168a78ee27f7ef297707a9a83028972b
Anushree <anushreejaiswal.srn@gmail.com>
2024-04-23 09:41:59 +0530

    created teams library

---
+++ lib/team.ts
@@ -0,0 +1,13 @@
+export const teamTypes = [
+    "GDSC Lead",
+    "Non-Technical Lead",
+    "Technical Leads",
+    "Android Development",
+    "Blockchain",
+    "Content Writing",
+    "Design & Videography",
+    "Event Management & Outreach",
+    "Machine Learning",
+    "Web Development",
+    "Women Tech Makers",
+];
