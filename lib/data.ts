f4067cdb55aa5a882f03388379de3afb0f031465
Anushree <anushreejaiswal.srn@gmail.com>
2024-04-23 09:40:56 +0530

    created data library

---
+++ lib/data.ts
@@ -0,0 +1,111 @@
+export const aboutContent = [
+    "Google collaborates with DSC Leads once a year who are pursuing an undergraduate or graduate university degree and supports them as they start and grow their on-campus community. DSC activities are targeted at University students and others including faculty members who want to learn development skills & work to solve real-life problems.",
+    "We at DSC — VIT Bhopal look forward to form a community where we are able to convert our knowledge into real time application, help each student to develop in different fields of technology and make use of our knowledge to build something that helps local businesses around us as well as their community.",
+];
+
+export const BOARD = [
+    {
+        marginLeft: "0",
+        color: "#ea4335",
+    },
+    {
+        marginLeft: "20px",
+        color: "#ea4335",
+    },
+    {
+        marginLeft: "40px",
+        color: "#fbbc05",
+    },
+    {
+        marginLeft: "60px",
+        color: "#fbbc05",
+    },
+    {
+        marginLeft: "80px",
+        color: "#34a853",
+    },
+    {
+        marginLeft: "100px",
+        color: "#34a853",
+    },
+    {
+        marginLeft: "120px",
+        color: "#4285f4",
+    },
+    {
+        marginLeft: "140px",
+        color: "#4285f4",
+    },
+];
+
+export const events = [
+    {
+        name: "ML Talks",
+        banner: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,w_500,h_500,g_center/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/events/WhatsApp%20Image%202024-01-18%20at%2021.13.13_86cc7027.jpg",
+        description:
+            "GDSC ML talks are focused on beginners and freshers in Machine Learning, aimed at getting them acquainted with ML and acting as a launchpad for their careers in ML.",
+        date: "Jan 19, 2024",
+        link: "https://gdsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-vit-bhopal-presents-gdsc-ml-talks/",
+    },
+    {
+        name: "Resume Building & Personal Branding",
+        banner: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,w_500,h_500,g_center/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/events/WhatsApp%20Image%202024-01-18%20at%2019.24.56_ac070c5f.jpg",
+        description:
+            "A session on maintaining a well-defined LinkedIn profile, resume, and portfolio which aims to improve your visibility to stand out from the crowd. This session will cover various aspects required to build a strong personal brand.",
+        date: "Jan 18, 2024",
+        link: "https://gdsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-vit-bhopal-presents-resume-building-and-personal-branding/",
+    },
+    {
+        name: "CodeToPlay",
+        banner: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,w_500,h_500,g_center/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/events/WhatsApp%20Image%202024-01-17%20at%2003.26.52_0f57edfa.jpg",
+        description:
+            "This session will guide participants through Flutter game development using the Flame engine, empowering them to create a classic Flappy Bird arcade game. Throughout the session, participants will build the game and gain insights into fundamental game development concepts, ensuring a hands-on experience and adopting best practices for achieving a seamless gaming interface.",
+        date: "Jan 17, 2024",
+        link: "https://gdsc.community.dev/events/details/developer-student-clubs-vellore-institute-of-technology-vit-bhopal-presents-winterfest-24-codetoplay/",
+    },
+];
+
+export const blogs = [
+    {
+        name: "WTM Newsletter: International Women’s Day Edition",
+        banner: "https://miro.medium.com/v2/resize:fit:828/format:webp/1*e5--8K1TctCdOgODW0dZqQ.png",
+        description:
+            "The power of Her: Celebrating the resilience and strength of women everywhere",
+        date: "Mar 8, 2023",
+        link: "https://medium.com/dsc-vit-bhopal/wtm-newsletter-international-womens-day-edition-be9a371983f5",
+    },
+    {
+        name: "From Suffrage to Solidarity: The Legacy of International Women’s Day",
+        banner: "https://miro.medium.com/v2/resize:fit:828/format:webp/0*62-OVEM_0geKy8VK.JPG",
+        description:
+            "International Women’s Day (IWD) is celebrated annually on March 8 to recognize the social, economic, cultural, and political achievements of women. The history of IWD dates back to the early 20th century when women began to demand better working conditions, voting rights, and other social and political reforms.",
+        date: "Mar 8, 2023",
+        link: "https://medium.com/dsc-vit-bhopal/from-suffrage-to-solidarity-the-legacy-of-international-womens-day-6f9e6edb5e99",
+    },
+    {
+        name: "GDSCZine 2021-22",
+        banner: "https://miro.medium.com/v2/resize:fit:828/format:webp/1*l8QfCUEkhWzrIWIiAlHP3w.png",
+        description:
+            "Google Developer Student Clubs (GDSCs) are university-based community groups for students interested in Google developer technologies. By joining a GDSC, students with a knack for technology enhance their knowledge in a peer-to-peer learning environment.",
+        date: "Jul 12, 2022",
+        link: "https://medium.com/dsc-vit-bhopal/gdsczine-2021-22-cdba915512a",
+    },
+];
+
+export const partners = [
+    {
+        name: "Wolfram",
+        banner: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/events/wolfram-corporate-logo-stacked-med_rYQwLfU.png",
+        link: "https://www.wolfram.com/?source=nav",
+    },
+    {
+        name: "Icons8",
+        banner: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/sponsors/2_OxlfJu3.png",
+        link: "https://icons8.com/",
+    },
+    {
+        name: "Interview Cake",
+        banner: "https://res.cloudinary.com/startup-grind/image/upload/c_fill,dpr_2.0,f_auto,g_center,q_auto:good/v1/gcs/platform-data-dsc/sponsors/3_aBVWLKU.png",
+        link: "https://www.interviewcake.com/",
+    },
+];
