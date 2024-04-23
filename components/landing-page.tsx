f3525398d8dfc9b7117f93f6aef9a002b1775e6b
Anushree <anushreejaiswal.srn@gmail.com>
2024-04-23 15:36:45 +0530

    updated the imports

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -1,18 +1,16 @@
 import { About } from "@/components/about/about";
-import { Connect } from "@/components/connect";
 import { LandingText } from "@/components/landing-text";
 import { Recent } from "@/components/recent";
-import { Team } from "./team";
+import { MeetTheTeam } from "./meet-the-team";
 import { Partners } from "./partners";
 export const LandingPage = () => {
     return (
         <>
             <LandingText />
             <About />
-            <Team />
+            <MeetTheTeam />
             <Recent />
             <Partners />
-            {/* <Connect /> */}
         </>
     );
 };
