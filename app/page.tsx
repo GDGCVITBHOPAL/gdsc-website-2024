cec6fb62a9ac666dcf7fbbdef4843e9d977e4593
Anushree <anushreejaiswal.srn@gmail.com>
2024-04-23 09:49:11 +0530

    updated the landing page

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { LandingPage } from "@/components/landing-page";
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen w-full bg-transparent transition-colors flex-col items-center">
+            <LandingPage />
+        </main>
+    );
+}
