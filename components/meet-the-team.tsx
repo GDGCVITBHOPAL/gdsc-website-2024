95fe48d243160ca66ce14c816fbcac3a89fea60c
Anushree <anushreejaiswal.srn@gmail.com>
2024-04-23 15:35:38 +0530

    updated the responsive styling of the component

--- components/meet-the-team.tsx
+++ components/meet-the-team.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 
-export const Team = () => {
+export const MeetTheTeam = () => {
     return (
-        <main className="min-h-screen w-full py-16 pr-28 my-[90px]">
-            <div className="relative w-full min-h-screen rounded-3xl border-[3px] border-black -ml-10 flex flex-col p-10 pl-16 overflow-hidden">
-                <div className="absolute top-[65px] -right-[40%] border-2 border-black w-full p-2 text-center text-lg font-medium shadow-[0px_5px_0px_0px_#000] rotate-[35deg]">
+        <main className="min-h-screen w-full py-16 pr-28 my-[90px] max-[1260px]:pr-20 max-[1100px]:pr-12 max-[880px]:pr-6 max-[700px]:pr-3">
+            <div className="relative w-full min-h-screen rounded-3xl border-[3px] border-black -ml-10 flex flex-col p-10 pl-16 overflow-hidden max-[1100px]:p-8 max-[800px]:p-5 max-[700px]:-ml-6">
+                <div className="absolute top-[65px] -right-[40%] bg-white border-2 border-black w-full p-2 text-center text-lg font-medium shadow-[0px_5px_0px_0px_#000] rotate-[35deg] max-[1300px]:text-base max-[1200px]:top-[60px] max-[1200px]:rotate-[40deg] max-[1080px]:text-sm max-[915px]:text-xs max-[835px]:rotate-45 max-[725px]:text-[10px] max-[700px]:mr-16 max-[700px]:-right-[50%] max-[700px]:top-[45px]">
                     Caffeine Powered Developers
                 </div>
-                <div className="text-5xl font-bold p-10">
+                <div className="text-5xl font-bold p-10 max-[700px]:pl-4">
                     <p className="text-gray-400 pb-3">Get to Know</p>
                     <p>the Team</p>
                 </div>
-                <p className="text-2xl font-semibold text-gray-400 w-[925px] p-10">
+                <p className="text-2xl font-semibold text-gray-400 w-[925px] p-10 max-[980px]:w-full max-[700px]:px-4">
                     Meet the crew behind{" "}
                     <span className="text-black">
                         Developer Student Clubs - VIT Bhopal!
@@ -23,7 +23,7 @@ export const Team = () => {
                     and tons of{" "}
                     <span className="text-black">collaborative projects!</span>
                 </p>
-                <div className="flex flex-wrap justify-between gap-10 p-10">
+                <div className="flex flex-wrap justify-between gap-10 p-10 max-[713px]:justify-center max-[700px]:px-4">
                     <div className="rounded-lg flex flex-col gap-5 w-[250px]">
                         <div className="relative w-[250px] h-[250px] bg-[#4285f4] bg-opacity-60 border-4 border-black rounded-xl">
                             <Image
