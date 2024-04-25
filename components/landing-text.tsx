4c153ee647be417511ec769fead14ed6c4f7ce2b
Anushree <anushreejaiswal.srn@gmail.com>
2024-04-25 13:59:39 +0530

    added background image and logo on the landing page

--- components/landing-text.tsx
+++ components/landing-text.tsx
@@ -1,12 +1,24 @@
 "use client";
 import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
 export const LandingText = () => {
     const { scrollYProgress } = useScroll();
     const x1 = useTransform(scrollYProgress, [0, 1], [50, -200]);
     const x2 = useTransform(scrollYProgress, [0, 1], [50, 200]);
     return (
-        <div className="w-full ">
-            <div className="h-[300px] w-full" />
+        <div className="w-full relative">
+            <div className="absolute top-0 left-0 h-screen w-full bg-white z-[-1]">
+                <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
+            </div>
+            <div className="h-[300px] w-full flex items-center justify-center">
+                <Image
+                    priority
+                    width={150}
+                    height={150}
+                    src={"/logo-design.svg"}
+                    alt="Follow us on Twitter"
+                />
+            </div>
 
             <div className="w-full overflow-hidden">
                 <motion.h1
