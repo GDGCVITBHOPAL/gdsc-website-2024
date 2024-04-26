084f57be66e8514b05dcf230b1180045c9e4e5e6
Anushree <anushreejaiswal.srn@gmail.com>
2024-04-26 12:38:04 +0530

    added all team members data

--- lib/team.ts
+++ lib/team.ts
@@ -11,3 +11,341 @@ export const teamTypes = [
     "Web Development",
     "Women Tech Makers",
 ];
+
+export const TeamData = [
+    {
+        domain: "GDSC Lead",
+        members: [
+            {
+                name: "Tanishka Mishra",
+                position: "GDSC Lead",
+                profileLink: "https://gdsc.community.dev/u/myp6ue/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114113/tanishka_mishra_9rZPnBp_-_ANUSHREE_JAISWAL_22BCE11109_xwaarx.webp",
+            },
+        ],
+    },
+    {
+        domain: "Non-Technical Lead",
+        members: [
+            {
+                name: "Shreya Sahu",
+                position: "Non-Technical Lead",
+                profileLink: "https://gdsc.community.dev/u/mjz8g6/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114098/20240424_123013_-_SHREYA_SAHU_22BCG10027_rql5hn.jpg",
+            },
+        ],
+    },
+    {
+        domain: "Technical Leads",
+        members: [
+            {
+                name: "Mohit Dhaker",
+                position: "Technical Lead",
+                profileLink: "https://gdsc.community.dev/u/mb9fu6/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114117/Mohit_Dhaker_profile_image_-_DHAKER_MOHIT_LAXMINARAYAN_21BCE11110_btg0zv.png",
+            },
+            {
+                name: "Shashwat Tiwari",
+                position: "Technical Lead",
+                profileLink: "https://gdsc.community.dev/u/mrjena/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114109/shashwat_tiwari_21bce11378_bXV5hpG_-_ANUSHREE_JAISWAL_22BCE11109_w3w1ah.webp",
+            },
+        ],
+    },
+    {
+        domain: "Android Development",
+        members: [
+            {
+                name: "Sparsh Rajput",
+                position: "Team Lead",
+                profileLink: "https://gdsc.community.dev/u/mrr675/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114118/sparsh_rajput_-_SPARSH_RAJPUT_21BCE11498_yvleay.jpg",
+            },
+            {
+                name: "Akash P",
+                position: "Team Co-Lead",
+                profileLink: "https://gdsc.community.dev/u/mncj4r/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114117/IMG_0354_-_AKASH_P_21BAI10403_gtpxmn.jpg",
+            },
+            {
+                name: "Abinash Dash",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/mbnnhx/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114100/IMG_20240202_123522_160_-_ABINASHA_DASH_22BAI10097_uk6myi.jpg",
+            },
+            {
+                name: "Ayan Gupta",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/mnubqf",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114114/IMG_20240425_183121_-_Ayan_Gupta_23bcy10295_nwfdlu.jpg",
+            },
+            {
+                name: "Gyan Vardhan",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/mwcryy/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114107/Gyan_Vardhan_22BCG10026_-_GYAN_VARDHAN_22BCG10026_e68xv4.jpg",
+            },
+            {
+                name: "Manoj Kumar Pradhan",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/m47xfm/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114113/Manoj_Kumar_-_MANOJ_KUMAR_PRADHAN_22BCE11415_logpug.jpg",
+            },
+        ],
+    },
+    {
+        domain: "Blockchain",
+        members: [
+            {
+                name: "Parth Vijay",
+                position: "Team Lead",
+                profileLink: "https://gdsc.community.dev/u/mjr5fn/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114111/Parth_Vijay_22BEY10121_Blockchain_Lead_-_PARTH_VIJAY_22BEY10121_izyb19.jpg",
+            },
+            {
+                name: "Sreya Nair",
+                position: "Team Co-Lead",
+                profileLink: "https://gdsc.community.dev/u/mpkyzf",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114112/IMG-20240107-WA0026_-_SREYA_NAIR_22BCE11004_s2puzv.jpg",
+            },
+            {
+                name: "Agnibha Chakraborty",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/mcqeam/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114110/IMG-20221008-WA0006_-_AGNIBHA_CHAKRABORTY_22MEI10045_cubnzp.jpg",
+            },
+            {
+                name: "Leonardo Fernandes",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/mnzufk/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114114/WhatsApp_-_LEONARDO_FERNANDES_22MEI10064_vlrzjt.jpg",
+            },
+            {
+                name: "Navya Bansal",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/m48hmm/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114113/img1.1_-_NAVYA_BANSAL_22BCE10225_vnx7rm.jpg",
+            },
+        ],
+    },
+    {
+        domain: "Content Writing",
+        members: [
+            {
+                name: "Akhil Thirunalveli",
+                position: "Team Lead",
+                profileLink: "https://gdsc.community.dev/u/m5veky/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114096/20240317_025341_-_THIRUNALVELI_AKHIL_BHASKAR_22BET10003_flqgga.jpg",
+            },
+            {
+                name: "Eshwari Anand Naidu",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/m6kc5y/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114110/IMG-20230405-WA0012_-_ESHWARI_ANAND_NAIDU_22BCE10974_ao6jez.jpg",
+            },
+            {
+                name: "Mansi Patwardhan",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/mwqrky/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114115/Mansi_Patwardhan_2_-_MANSI_PATWARDHAN_22BSA10301_v9im1g.jpg",
+            },
+        ],
+    },
+    {
+        domain: "Design & Videography",
+        members: [
+            {
+                name: "Riti Sinha",
+                position: "Team Lead",
+                profileLink: "https://gdsc.community.dev/u/mc47pb/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714115054/IMG_1697_-_RITI_SINHA_22BSA10269_x3rclc.png",
+            },
+            {
+                name: "Alok Kumar Mishra",
+                position: "Team Co-Lead",
+                profileLink: "https://gdsc.community.dev/u/mrphks/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114108/self1_-_ALOK_KUMAR_MISHRA_22BSA10172_cjwkdm.jpg",
+            },
+            {
+                name: "Aditya Srivastava ",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/mp5zyv/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114106/PHOTO_-_Aditya_Srivastava_23bec10029_urldmz.png",
+            },
+            {
+                name: "Shreya Upadhyay",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/mv4ga9/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114112/Shreya_Upadhyay_-_SHREYA_UPADHYAY_22BCE10924_mhzgwa.jpg",
+            },
+        ],
+    },
+    {
+        domain: "Event Management & Outreach",
+        members: [
+            {
+                name: "Atharv Sathe",
+                position: "Team Lead",
+                profileLink: "https://gdsc.community.dev/u/mj96nk/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114107/ProfilePic1_Edit_1_-_ATHARV_SATHE_22BCE10388_lbjrht.jpg",
+            },
+            {
+                name: "Naman Kundan",
+                position: "Team Co-Lead",
+                profileLink: "https://gdsc.community.dev/u/mvsre2/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114115/Naman_-_NAMAN_KUNDAN_22BCE10598_olunxj.jpg",
+            },
+            {
+                name: "Anusha Ajith Sreeja",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u//",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114106/Screenshot_20240105-160627_Gallery_-_ANUSHA_A.S_22BCE11374_i0czj1.jpg",
+            },
+            {
+                name: "Divij Ganjoo",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114123/IMG20230709152404_-_DIVIJ_GANJOO_22BCE11126_e9yptb.jpg",
+            },
+            {
+                name: "Parth Bhanti",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u//",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114117/IMG_20240425_140043_-_Parth_Bhanti_23BCE11758_q8qu4r.jpg",
+            },
+        ],
+    },
+    {
+        domain: "Machine Learning",
+        members: [
+            {
+                name: "Aditya Paul",
+                position: "Team Lead",
+                profileLink: "https://gdsc.community.dev/u/m6qpfa/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114100/pic_-_ADITYA_PAUL_22BCE10030_xw3n8h.jpg",
+            },
+            {
+                name: "Punit Kaushik",
+                position: "Team Co-Lead",
+                profileLink: "https://gdsc.community.dev/u/mwfczp/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114096/1703664938085_-_PUNIT_KAUSHIK_22BCE11400_iplddn.jpg",
+            },
+            {
+                name: "Arihant Bhandari",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/mru9kr/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114097/Arihant_Bhandari_-_ARIHANT_BHANDARI_22BCE11373_vs8ur1.jpg",
+            },
+            {
+                name: "Sayantika Laskar",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/m8mx6v/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114116/Sayantika_-_SAYANTIKA_LASKAR_22BHI10151_efud60.jpg",
+            },
+            {
+                name: "Suhani Thakur",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/suhani_thakur/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114118/Suhani_Thakur_-_SUHANI_THAKUR_22BAI10078_eqlksw.jpg",
+            },
+        ],
+    },
+    {
+        domain: "Web Development",
+        members: [
+            {
+                name: "Anushree Jaiswal",
+                position: "Team Lead",
+                profileLink: "https://gdsc.community.dev/u/m674pc/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114103/B612_20230826_204611_030_-_ANUSHREE_JAISWAL_22BCE11109_t9q8yf.jpg",
+            },
+            {
+                name: "Khushal Bhardwaj",
+                position: "Team Co-Lead",
+                profileLink: "https://gdsc.community.dev/u/mzdc85/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114114/celeroncoder-compressed_-_KHUSHAL_BHARDWAJ_22BCE10691_d4uivk.png",
+            },
+            {
+                name: "Ishank Lalwani",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/myyrtn",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114095/1645424893739_-_ISHANK_LALWANI_22BCE10338_mmzkio.jpg",
+            },
+            {
+                name: "Shashwat Dimri",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u//",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114114/A5D71261-75F6-4CBF-A452-084ABF3C2033_-_SHASHWAT_DIMRI_22BCE11105_sesw1j.jpg",
+            },
+            {
+                name: "Shubh Mehrotra",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/m42r5e/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114118/Shubhpic_-_SHUBH_MEHROTRA_22BCE10561_j4htyp.jpg",
+            },
+        ],
+    },
+    {
+        domain: "Women Tech Makers",
+        members: [
+            {
+                name: "Neelakshi Choudhary",
+                position: "Team Lead",
+                profileLink: "https://gdsc.community.dev/u/mc9f4j/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114119/profile_pic_2_-_NEELAKSHI_CHOUDHARY_22BAI10263_dfysjn.jpg",
+            },
+            {
+                name: "Radha Agarwal",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/m6bcye/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114117/Screenshot_2024_0425_181801_-_RADHA_AGARWAL_22BSA10089_rthwx5.jpg",
+            },
+            {
+                name: "Shreya Agrawal",
+                position: "Team Member",
+                profileLink: "https://gdsc.community.dev/u/m6pzaa/",
+                profileImage:
+                    "https://res.cloudinary.com/db7nrltsv/image/upload/v1714114095/1000037709_-_Shreya_Agrawal_23BEC10082_gv5brg.jpg",
+            },
+        ],
+    },
+];
