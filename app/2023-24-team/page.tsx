e7b50c9df1298aa495a0118fd0d5bd1eaa841001
Anushree <anushreejaiswal.srn@gmail.com>
2024-04-27 08:45:32 +0530

    updated styling

--- app/2023-24-team/page.tsx
+++ app/2023-24-team/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Page() {
     return (
-        <main className="relative w-screen min-h-screen">
+        <main className="relative w-screen min-h-screen select-none">
             <div className="p-10 flex flex-col items-center">
                 <div className="fixed top-0 left-0 h-screen w-full bg-white z-[-1]">
                     <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)]"></div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { LandingPage } from "@/components/landing-page";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen w-full bg-transparent transition-colors flex-col items-center">
+        <main className="flex min-h-screen w-full bg-transparent transition-colors flex-col items-center select-none">
             <LandingPage />
         </main>
     );
