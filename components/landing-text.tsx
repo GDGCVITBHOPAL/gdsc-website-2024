422407afd5a175c6a76022d0ddc154aabbc1e090
Anushree <anushreejaiswal.srn@gmail.com>
2024-05-06 10:31:59 +0530

    replaced h1 tage with div

--- components/landing-text.tsx
+++ components/landing-text.tsx
@@ -21,15 +21,15 @@ export const LandingText = () => {
             </div>
 
             <div className="w-full overflow-hidden">
-                <motion.h1
+                <motion.div
                     style={{ x: x1 }}
                     className="w-full flex justify-center text-[10vw] text-gray-700 whitespace-nowrap transition-all max-[600px]:text-5xl"
                 >
                     gle Google Google&nbsp;
                     <span className="text-[#ea4335] font-medium">Google</span>
                     &nbsp;Google Google Google
-                </motion.h1>
-                <motion.h1
+                </motion.div>
+                <motion.div
                     style={{ x: x2 }}
                     className="w-full mt-[-3.5vw] flex justify-center text-[10vw] text-gray-700 whitespace-nowrap transition-all max-[600px]:text-5xl max-[600px]:mt-2 max-[600px]:ml-[-100px]"
                 >
@@ -38,23 +38,23 @@ export const LandingText = () => {
                         Developer
                     </span>
                     &nbsp;Developer Develop
-                </motion.h1>
-                <motion.h1
+                </motion.div>
+                <motion.div
                     style={{ x: x1 }}
                     className="w-full mt-[-3.5vw] flex justify-center text-[10vw] text-gray-700 whitespace-nowrap transition-all max-[600px]:text-5xl max-[600px]:mt-2"
                 >
                     ent Student Student&nbsp;
                     <span className="text-[#34a853] font-medium">Student</span>
                     &nbsp;Student Student Student
-                </motion.h1>
-                <motion.h1
+                </motion.div>
+                <motion.div
                     style={{ x: x2 }}
                     className="w-full mt-[-3.5vw] flex justify-center text-[10vw] text-gray-700 whitespace-nowrap transition-all max-[600px]:text-5xl max-[600px]:mt-2 max-[600px]:ml-[-150px]"
                 >
                     Club Club Club Club Club&nbsp;
                     <span className="text-[#fbbc05] font-medium">Club</span>
                     &nbsp;Club Club Club Club
-                </motion.h1>
+                </motion.div>
 
                 <div className="w-full text-[1.8vw] text-center leading-tight font-semibold max-[600px]:text-2xl max-[600px]:mt-6">
                     x VIT Bhopal
